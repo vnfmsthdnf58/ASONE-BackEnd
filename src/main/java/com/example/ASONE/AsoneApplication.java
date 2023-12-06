3434a3d8791f14e0c0c1197e61655e0b6fda6eb8
DESKTOP-T6SRFBK\Administrator <thdnf586@naver.com>
2023-12-06 13:12:34 +0900

    프로젝트 초안

---
+++ src/main/java/com/example/ASONE/AsoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.ASONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ASONE/AsoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ASONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
